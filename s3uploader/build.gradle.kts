e72145daf7637ecf40cc73dc870b1f8ef933bee6
Amanullah <amanullah@ozi.com>
2024-10-16 12:45:44 +0500

    Production Release v_1.1.9

--- s3uploader/build.gradle.kts
+++ s3uploader/build.gradle.kts
@@ -63,7 +63,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.aman.s3uploader"
                 artifactId = "s3uploader"
-                version = "v_1.1.8"
+                version = "v_1.1.9"
 
                 from(components["release"])
             }
